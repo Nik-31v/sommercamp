d9d2190b9557cbdd9086a8109d1001b022daf678
Nik-31v <niklas.maroldt@gmx.de>
2025-07-04 14:30:28 +0000

    finished snipped editor

---
+++ sommercamp/snippet_editor.py
@@ -0,0 +1,22 @@
+import json
+import re
+
+input_file_path = "data/documents.jsonl"
+output_file_path = "data/documents_edited.jsonl"
+
+with open(input_file_path, "r") as input_file, open(output_file_path, "w") as output_file:
+    for line in input_file:
+        line_data = json.loads(line)
+        text = line_data.get("text")
+        text = (text[text.find("Gesamtzeit"):text.find("Rezept von")])
+
+        saetze = re.findall(r'.*?\. ?', text)
+        char_amount = sum(len(satz) for satz in saetze)
+
+        while char_amount > 475:
+            del saetze[-1]
+            char_amount = sum(len(satz) for satz in saetze)
+        text_output = "".join(saetze)
+
+        line_data["text_visible"] = text_output
+        output_file.write(json.dumps(line_data) + "\n")
